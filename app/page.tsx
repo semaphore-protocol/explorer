a2df0011582779146940a56d4c1e5fd90fc83ed8
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-04-16 15:37:28 -0500

    chore: :heavy_plus_sign: Add `@semaphore-protocol/data`

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const dmSans = DM_Sans({ weight: ["400", "500", "700"], subsets: ["latin"] });
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className={`${dmSans.className} font-bold text-4xl`}>
+            <h1 className={`${dmSans.className} font-bold text-5xl`}>
                 Semaphorus
             </h1>
         </main>
