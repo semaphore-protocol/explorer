23e82b52f9a053b3069ea62fca59b29bef8798c6
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-04-16 15:42:51 -0500

    chore: :lipstick: Change font to Poppins

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import Image from 'next/image'
-import { DM_Sans } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-const dmSans = DM_Sans({ weight: ["400", "500", "700"], subsets: ["latin"] });
+const mainFont = Poppins({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
 export default function Home() {
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className={`${dmSans.className} font-bold text-5xl`}>
+            <h1 className={`${mainFont.className} font-medium text-5xl`}>
                 Semaphorus
             </h1>
         </main>
