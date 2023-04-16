334cf9344b6a76bb2823d15000d48a2e964543fd
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-04-16 15:58:21 -0500

    chore: :heavy_plus_sign: Add and setup style plugins

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import { Poppins } from "next/font/google";
 const mainFont = Poppins({ weight: ["400", "500", "700"], subsets: ["latin"] });
 
 export default function Home() {
-    return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <h1 className={`${mainFont.className} font-medium text-5xl`}>
-                Semaphorus
-            </h1>
-        </main>
-    );
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className={`${mainFont.className} text-4xl font-medium`}>
+        Semaphorus
+      </h1>
+    </main>
+  );
 }
---
+++ utils.ts
@@ -0,0 +1,6 @@
+import clsx, { ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function tw(...inputs: ClassValue[]) {
+    return twMerge(clsx(inputs));
+}
