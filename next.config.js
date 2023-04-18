2900595b61c5b9660d3c86b80119fda26ae81bef
cedoor <me@cedoor.dev>
2023-04-18 19:52:50 +0200

    fix: update base path

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    basePath: "/semaphore-explorer"
+    reactStrictMode: true
 }
 
 module.exports = nextConfig
