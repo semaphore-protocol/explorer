a54e4024527390f26c9abdbd3c0c4b4b4381a0e1
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-04-21 00:41:43 +0900

    feat: :sparkles: Add search

--- components/ui/Search.tsx
+++ components/ui/Search.tsx
@@ -1,16 +1,20 @@
-"use client";
+import { ChangeEvent } from "react";
 import { Input } from "./Input";
 
-export default function Search() {
+export default function Search({
+  setSearchTerm: updateSearchTerm,
+}: {
+  setSearchTerm: (newTerm: string) => void;
+}) {
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) =>
+    updateSearchTerm((e.target as HTMLInputElement).value);
+
   return (
     <div className="flex items-center justify-center gap-3">
-      <Input
-        placeholder="Search by id"
-        onKeyDown={(e) => {
-          if (e.code === "Enter") console.log("pressed");
-        }}
-      />
-      <button className="rounded-md bg-slate-500 p-3">Search</button>
+      <Input placeholder="Search by id" onChange={handleChange} />
+      <button className="rounded-md bg-slate-200 p-3 font-semibold text-slate-600">
+        Search
+      </button>
     </div>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
+import { GroupResponse } from "@semaphore-protocol/data";
 import clsx, { ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function tw(...inputs: ClassValue[]) {
-    return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs));
 }
+
+export const searchGroupById = (groups: GroupResponse[], searchTerm: string) =>
+  groups.filter((group) => group.id.includes(searchTerm));
