cb1a3b0cf66bcd3adc61c320a544218530a90b3f
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-04-21 02:17:34 +0900

    fix: :adhesive_bandage: Fix color name

--- components/StatCard/index.tsx
+++ components/StatCard/index.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 interface StatCardProps {
   title: string;
   value: string;
@@ -6,7 +8,7 @@ interface StatCardProps {
 export default function StatCard({ title, value }: StatCardProps) {
   return (
     <div className="flex flex-1 flex-col gap-4 rounded-lg border border-slate-500 bg-slate-50 p-4 dark:border-slate-600 dark:bg-slate-900">
-      <p className="text-xs font-medium uppercase tracking-widest text-gray-600 dark:text-gray-300">
+      <p className="text-xs font-medium uppercase tracking-widest text-slate-600 dark:text-slate-300">
         {title}
       </p>
       <h1>{value}</h1>
