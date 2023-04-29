1f27641922488b988c88f3375047a1ce83e8ba01
thebeyondr <19380973+thebeyondr@users.noreply.github.com>
2023-04-29 17:08:42 +0900

    feat: :sparkles: Format `ids` if they're too long

--- components/ui/GroupCard.tsx
+++ components/ui/GroupCard.tsx
@@ -1,13 +1,14 @@
 import { GroupWithNetwork } from "@/lib/types";
+import { formatId } from "@/lib/utils";
 
 export default function GroupCard({ group }: { group: GroupWithNetwork }) {
   return (
-    <div className="flex flex-col justify-between gap-3 rounded-md border border-slate-700 bg-slate-800 p-3 text-sm transition lg:flex-row lg:items-center lg:text-base">
+    <div className="flex flex-col justify-between gap-3 rounded-md border border-slate-700 bg-slate-800 p-3 text-sm transition hover:cursor-pointer hover:bg-slate-700 lg:flex-row lg:items-center lg:text-base">
       <div className="flex gap-2">
         <p className="rounded-2xl border border-amber-200 px-2 font-medium text-slate-300">
-          {`ID: ${group.id}`}
+          {`ID: ${formatId(group.id)}`}
         </p>
-        <p className="rounded-2xl bg-slate-700 px-2 text-slate-300">
+        <p className="rounded-2xl bg-slate-600 px-2 text-slate-300">
           🔗 {group.network}
         </p>
       </div>
--- lib/utils.ts
+++ lib/utils.ts
@@ -10,3 +10,13 @@ export const searchGroupById = (
   groups: GroupWithNetwork[],
   searchTerm: string
 ) => groups.filter((group) => group.id.includes(searchTerm));
+
+// If the id length is over 16 characters, shorten it to 16 characters and add "..." at the end.
+export const formatId = (id: string) => {
+  const maxIdLength = 10;
+  if (id.length > maxIdLength) {
+    return id.slice(0, 7) + "...";
+  } else {
+    return id;
+  }
+};
